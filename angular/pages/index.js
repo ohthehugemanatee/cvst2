c83c7465ffa410ca3a638a2fd6774f1046ac6da9
Campbell Vertesi <campbell@vertesi.com>
2015-03-10 17:37:10 +0100

    re-architect to use scope to track UID.

--- angular/pages/index.js
+++ angular/pages/index.js
@@ -15,22 +15,65 @@
 
     });
 
-    app.directive('userForm', ['$http', function($http) {
+    /**
+     * Service: DrupalOrgUser
+     * Get a drupal user object from UID.
+     */
+    app.factory('drupalOrgUser', ['$http', '$q', function ($http, $q) {
+        // Get a user.
+        return function (uid) {
+            // If this was a non-numeric UID, reject the promise.
+            if (!isFinite(uid)) {
+                return $q.reject(new TypeError("UID should be numeric, got" + uid));
+            }
+
+            // since the UID is numeric, let's make our API call.
+            $request = $http.get('https://www.drupal.org/api-d7/user.json', {
+                params: {
+                    uid: +uid
+                }
+            });
+            // When request changes state, either return the user object or reject.
+            $request.then(function(data) {
+               if (data.data.list[0]) {
+                   // if there's a result set, take the first result as our user object.
+                   return data.data.list[0];
+               } else {
+                   // even though $http.get was successful, we got no results. What a let down.
+                   return $q.reject(new Error("No user with uid " + uid));
+               }
+            }, function(reason) {
+                // http request failed, apparently.
+                return $q.reject(new Error("HTTP request failed with reason: " + reason));
+            });
+        }
+    }]);
+
+    /**
+     * Directive: UserController
+     * Take the form input Event and use it to dump the user object into $scope.
+     */
+    app.controller('UserController', ['$scope', 'drupalOrgUser', function($scope, getUser) {
+        $scope.selectUser = function($event) {
+            // Don't fire if it's default value, or if the form is invalid.
+            $event.preventDefault();
+
+            if (!$scope.userForm.$valid) {
+                return;
+            }
+
+            getUser($scope.uid).then(function(user) {
+                $scope.user = user;
+            });
+        }
+    }]);
+
+    app.directive('userForm', function() {
         return {
             restrict: 'E',
-            templateUrl: 'angular/components/user-form.html',
-            controller: function() {
-                this.submission = {};
-                this.selectUser = function(target) {
-                    newuser = $http.get('https://www.drupal.org/api-d7/user.json?uid=' + this.submission.uid).success(function(data) {
-                        target = data.list[0];
-                    });
-                    this.submission = {};
-                };
-            },
-            controllerAs: 'userFormCtrl'
+            templateUrl: 'angular/components/user-form.html'
         };
-    }]);
+    });
 
     app.directive('userInfo', function() {
        return {
