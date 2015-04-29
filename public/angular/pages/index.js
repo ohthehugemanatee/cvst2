61ff4ad6506c2cd0830e4479c99485842bb555e8
Campbell Vertesi <campbell@vertesi.com>
2015-04-29 12:12:04 -0400

    update js for getting states... in theory

--- public/angular/pages/index.js
+++ public/angular/pages/index.js
@@ -9,28 +9,23 @@
      * Service: DrupalOrgUser
      * Get a drupal user object from UID.
      */
-    app.factory('drupalOrgUser', ['$http', '$q', function ($http, $q) {
+    app.factory('USStates', ['$http', '$q', function ($http, $q) {
         // Get a user.
-        return function (uid) {
-            // If this was a non-numeric UID, reject the promise.
-            if (!isFinite(uid)) {
-                return $q.reject(new TypeError("UID should be numeric, got" + uid));
-            }
-
-            // since the UID is numeric, let's make our API call.
-            var request = $http.get('https://www.drupal.org/api-d7/user.json', {
+        return function (stateid) {
+            // Make the API call.
+            var request = $http.get('http://10.11.12.14/states.json', {
                 params: {
-                    uid: +uid
+                    stateid: +stateid
                 }
             });
             // When request changes state, either return the user object or reject.
             return request.then(function(data) {
                if (data.data.list[0]) {
-                   // if there's a result set, take the first result as our user object.
+                   // if there's a result set, take the first result as our state object.
                    return data.data.list[0];
                } else {
                    // even though $http.get was successful, we got no results. What a let down.
-                   return $q.reject(new Error("No user with uid " + uid));
+                   return $q.reject(new Error("No state with state ID " + stateid));
                }
             }, function(reason) {
                 // http request failed, apparently.
@@ -40,20 +35,23 @@
     }]);
 
     /**
-     * Directive: UserController
+     * Directive: USStateController
      * Take the form input Event and use it to dump the user object into $scope.
      */
-    app.controller('UserController', ['$scope', 'drupalOrgUser', function($scope, getUser) {
-        $scope.selectUser = function($event) {
-            // Don't fire if it's default value, or if the form is invalid.
+    app.controller('USStateController', ['$scope', 'USStates', function($scope, getState) {
+        $scope.selectStates = function($event) {
+            // Don't fire if it's the default value, or if the form is invalid.
             $event.preventDefault();
-
-            if (!$scope.userForm.$valid) {
+            if (!$scope.usStateForm.$valid) {
                 return;
             }
 
-            getUser($scope.uid).then(function(user) {
-                $scope.user = user;
+            getState($scope.stateid1).then(function(state) {
+                $scope.state1 = state;
+            });
+
+            getState($scope.stateid2).then(function(state) {
+                $scope.state2 = state;
             });
         }
     }]);
