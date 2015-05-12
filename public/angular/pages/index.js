fb0c4c0b373366356346640408acd3961c605a03
Campbell Vertesi <campbell@vertesi.com>
2015-05-11 21:24:34 -0700

    clean out index.js

--- public/angular/pages/index.js
+++ public/angular/pages/index.js
@@ -9,7 +9,7 @@
      * Controller: USStateController
      * Initialize variables.
      */
-    app.controller('USStateController', ["$scope", 'USStatesList', function($scope, getStatesList) {
+    app.controller('USStateController', ["$scope", function($scope) {
         $scope.stateslist = [
             'Alabama',
             'Arkansas',
@@ -34,7 +34,7 @@
                 statedata: '=',
                 winner: '='
             },
-            controller: function($scope, USStates, winnerTally) {
+            controller: function($scope, winnerTally) {
                 $scope.selectState = function ($event) {
                     // Don't fire if it's the default value, or if the form is invalid.
                     $event.preventDefault();
@@ -44,25 +44,29 @@
 
                     // Don't fire if the two states are identical, or if one is undefined.
                     if ($scope.stateid1 == $scope.stateid2
-                    || !$scope.stateid1
-                    || !$scope.stateid2) {
+                        || !$scope.stateid1
+                        || !$scope.stateid2) {
                         return;
                     }
 
-                    USStates.getter($scope.stateid1).then(function (data) {
-                        var state = USStates.reducer(data);
-                        $scope["statedata"]["state1"] = state;
-                        $scope["winner"] = winnerTally.tally($scope["statedata"]["state1"], $scope["statedata"]["state2"]);
-                    });
+                    $scope["statedata"] = {
+                        "state1": {
+                            "statenum": 1,
+                            "name": $scope.stateid1,
+                            "field_fiber": '12345',
+                            "field_singles": '99999',
+                            "field_toilets" : '12345'
+                        },
+                        "state2": {
+                            "statenum": 2,
+                            "name": $scope.stateid2,
+                            "field_fiber": '55555',
+                            "field_singles": '12345',
+                            "field_toilets" : '99999'
+                        }
+                    };
 
-                    USStates.getter($scope.stateid2).then(function (data) {
-                        var state = USStates.reducer(data);
-                        $scope["statedata"]["state2"] = state;
-                        $scope["winner"] = winnerTally.tally($scope["statedata"]["state1"], $scope["statedata"]["state2"]);
-                    });
-
-
-
+                    $scope["winner"] = winnerTally.tally($scope["statedata"]["state1"], $scope["statedata"]["state2"]);
                 }
             }
         }
@@ -142,89 +146,5 @@
         };
         return service;
     });
-
-
-    /**
-     * Service: USStatesList
-     * Get a US statelist.
-     **/
-    app.factory('USStatesList', ['$http', '$q', function ($http, $q) {
-        return function() {
-            return [
-                'Alabama',
-                'Arkansas',
-                'Hawaii'
-            ];
-        }
-
-    }]);
-
-
-    /**
-     * Service: USStates
-     * Get a US state object from state ID.
-     */
-    app.factory('USStates', ['$http', '$q', function ($http, $q) {
-        var service = {};
-        service.getter = function (stateid) {
-            // Make the API call.
-            var request = $http.get('http://cvst-backend.dev.nodesymphony.com/state-data/', {
-                params: {
-                    stateid: +stateid
-                }
-            });
-            // When request changes state, either return the state object or reject.
-            return request.then(function(result) {
-                if (result.data) {
-                    // If there's a result set, return it.
-                    return result.data;
-                } else {
-                    // even though $http.get was successful, we got no results. What a let down.
-                    return $q.reject(new Error("No state with state ID " + stateid));
-                }
-            }, function(reason) {
-                // http request failed, apparently.
-                return $q.reject(new Error("HTTP request failed with reason: " + reason));
-            });
-        };
-
-        // Takes a multi-node result set and reduces it down to a single object, handling duplicate fields reasonably.
-        service.reducer = function (data) {
-            var reduced = {};
-            // Iterate over the nodes.
-            for (var result in data) {
-                if (data[result]) {
-                    // Iterate over the fields.
-                    for (key in data[result]) {
-                        // If the field isn't empty, add it to the "reduced" array.
-                        if (data[result][key].length > 0) {
-                            var value = data[result][key];
-                            if (!reduced[key]) {
-                                // If we don't have a value for this field yet, just set it.
-                                reduced[key] = value;
-                            }
-                            else {
-                                // We already have a value for this field, so try appending numbers to the field name until
-                                // we find one that's not set yet. So duplicate values will look like reduced.field_value and
-                                // reduced.field_value2.
-                                var i = 0;
-                                while (data[result][key]) {
-                                    var ourkey = key +i;
-                                    if (!reduced[ourkey]) {
-                                        reduced[ourkey] = value;
-                                        data[result][key] = null;
-                                    }
-                                    i++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return reduced;
-        };
-        return service;
-
-    }]);
 
 })();
